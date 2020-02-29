2ba8560e31a7f746093488efb34581cb38958b67
Xande0812 <xande0812@gmail.com>
2020-02-29 01:04:39 +0000

    alias

--- config/fish/conf.d/aliases.fish
+++ config/fish/conf.d/aliases.fish
@@ -1,6 +1,7 @@
 # aliases
 alias vi='nvim'
 alias vim='nvim'
-# alias ls='exa -ahl --git'
-# alias tree='exa -T --git-ignore'
 alias k='kubectl'
+
+alias ls='exa -ahl --git'
+alias tree='exa -T --git-ignore'
