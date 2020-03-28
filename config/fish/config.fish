d781859bf5d4ceea98e81b5a266fbcc6f34710a3
Xande0812 <xande0812@gmail.com>
2020-03-28 12:08:43 +0000

    locale設定追加

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,8 @@
+# locale設定
+set LANG ja_JP.UTF-8
+set LANGUAGE "ja_JP:ja"
+
+# fisher
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
