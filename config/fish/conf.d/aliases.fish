cf427c1f1065ed374eb5cb9e321f4301861d2270
Xande0812 <xande0812@gmail.com>
2021-09-04 01:30:46 +0900

    exaのalias増やした

--- config/fish/conf.d/aliases.fish
+++ config/fish/conf.d/aliases.fish
@@ -5,6 +5,16 @@ alias k='kubectl'
 alias d='docker'
 alias dc='docker-compose'
 
-alias ls='exa -ahl --git'
+alias e='exa --icons --git'
+alias ls=e
+alias ea='exa -a --icons --git'
+alias la=ea
+alias ee='exa -aahl --icons --git'
+alias ll=ee
+alias et='exa -T -L 3 -a -I "node_modules|.git|.cache" --icons'
+alias lt=et
+alias eta='exa -T -a -I "node_modules|.git|.cache" --color=always --icons | less -r'
+alias lta=eta
+alias l='clear && ll'
 
 alias lg='lazygit'
